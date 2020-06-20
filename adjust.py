8f3c0e35c7614aedf1b15af64731224cb147408e
Madhav Wagle <architwagle@gmail.com>
2020-06-21 01:04:42 +0530

    additional sample

--- adjust.py
+++ adjust.py
@@ -9,8 +9,8 @@ def save_obj(obj, name ):
     with open('obj/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-
-fn = 'images/sample2.jpeg'
+storage_dict = []
+fn = 'images/sample_nil.jpeg'
 imgray = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
 
 cv2.namedWindow('image')
@@ -34,16 +34,54 @@ def is_pixel_column_white(x,T):
             return False
     return True
 
+def final_vertical_allignment():
+    
+    
+    alpha = storage_dict[0]
+    short = ['g','j','p','q','y']
+    average = ['a','c','e','i','m','n','o','r','s','u','v','w','x','z']
+    tall = ['b','d','f','h','k','l','t']
+    print("hi")
+    tallest = -1
+    for ch in tall:
+        if(alpha[ord(ch)-97].shape[0] > tallest):
+            tallest = alpha[ord(ch)-97].shape[0] 
+    shortest = -1
+    for ch in short:
+        if(alpha[ord(ch)-97].shape[0] > shortest):
+            shortest = alpha[ord(ch)-97].shape[0]     
+    
+    summ = 0
+    for ch in average:
+            summ += alpha[ord(ch)-97].shape[0]
+    average_height = summ// len(average)
 
-
+    
+    diff_t = tallest - average_height
+    diff_s = shortest - average_height
+    
+    for i in range(26):
+        ch = chr(i+97)
+        if ch not in tall:
+            padd = np.zeros([diff_t,alpha[i].shape[1]],dtype=np.uint8)
+            padd.fill(255)
+            alpha[i] = cv2.vconcat([padd, alpha[i]])
+        
+        if ch not in short:
+            padd = np.zeros([diff_s,alpha[i].shape[1]],dtype=np.uint8)
+            padd.fill(255)
+            alpha[i] = cv2.vconcat([alpha[i],padd])
+    storage_dict[0] = alpha
+    save_obj(storage_dict,"storage_dict")
+    
 def process_characters(x, i, T, img_crop):
     
     # remove space from top and below 
-    
     y_top = 0
     while  y_top<len(img_crop):
         if is_pixel_row_white(img_crop[y_top],T):
             y_top+=1
+            print("x")
         else:
             break
     
@@ -52,13 +90,16 @@ def process_characters(x, i, T, img_crop):
         if is_pixel_row_white(img_crop[y_bottom],T):
             y_bottom-=1
         else:
-            break        
-        
+            break  
+          
+    #print(img_crop)
     img_crop = img_crop[y_top:y_bottom, 0:img_crop.shape[1]]
+
     return img_crop
 
 def apply_segmentation(img,T, save):
     
+    global storage_dict
     font = cv2.FONT_HERSHEY_SIMPLEX
     
     # types of characters available, along with their numbers
@@ -149,11 +190,12 @@ def apply_segmentation(img,T, save):
                 storage_dict[x][i]=  process_characters(x, i , T,img_backup[temp[0][0]:temp[1][0]+1,temp[0][1]:temp[1][1]+1])
 
                 while True:
+                    break
                     #print(temp[0][1],temp[1][1]+1,temp[0][0],temp[1][0]+1)
                     #cv2.imshow('image', img_backup[temp[0][0]:temp[1][0]+1, temp[0][1]:temp[1][1]+1])
-                    cv2.imshow('image', storage_dict[x][i])
-                    if cv2.waitKey(10) == 27:
-                        break
+                    #cv2.imshow('image', storage_dict[x][i])
+                    #if cv2.waitKey(10) == 27:
+                    #    break
             else:
                 cv2.rectangle(img, pt1=(temp[0][1],temp[0][0]), pt2=(temp[1][1],temp[1][0]),color=(0,255,255),thickness=1)  
     if save:
@@ -163,6 +205,7 @@ def apply_segmentation(img,T, save):
 
 t = -1
 s = -1
+
 while(1):
     t = cv2.getTrackbarPos('T','image')
     s = cv2.getTrackbarPos(switch,'image')
@@ -170,7 +213,7 @@ while(1):
     (thresh, img) = cv2.threshold(imgray, t, 255, cv2.THRESH_BINARY)
     
     if(s==1): 
-        img1 = apply_segmentation(img,t, False)
+        img1 = apply_segmentation(img.copy(),t, False)
         cv2.imshow('image',img1)
     else:
         cv2.imshow('image',img)
@@ -178,7 +221,9 @@ while(1):
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
-if(s==1):
+        
+if s==1:
     apply_segmentation(img,t, True)
-cv2.destroyAllWindows()
-
+    print("hi")
+    final_vertical_allignment()
+    cv2.destroyAllWindows()
--- hw.py
+++ hw.py
@@ -8,31 +8,67 @@ def load_obj(name ):
     with open('obj/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
         
-        
-hw_dict= load_obj("storage_dict")[0]
-
+s_dict  =  load_obj("storage_dict")       
+hw_dict= s_dict[0]
+pc_dict= s_dict[1]
+pc = {"-":0,".":1,",":2,"/":3,"\\":4,":":5,"&":6,"%":7,"^":8,"(":9,")":10,"[":11,"]":12,"@":13,"#":14,"!":15}
+nm_dict = s_dict[2]
+#nm = {"1":1}
 f = open("input.txt", "r")
 print(len(hw_dict))
 ans = []
 max_len = -1
+'''
+paragh = []
+count = 0
+for line in f:
+    temp_ans = []
+    for x in line:
+        count+=1
+        temp_ans.append(x)
+        if(count%80==0):
+            paragh.append("".join(temp_ans.copy()))
+            temp_ans = []
+    paragh.append("".join(temp_ans.copy()))
+      
+for i in paragh:
+    print(i)
+f = paragh
+
+'''
 for line in f:
     print(line)
     line = line.rstrip().lower()
-    im_v = hw_dict[ord(line[0])-97].copy()
-    for x in line[1::]:
+    
+    start = 0
+    while(line[start]==" "):
+        start+=1
+    
+    if ord(line[start])-97 in hw_dict:
+        im_v = hw_dict[ord(line[start])-97].copy()
+    elif line[start] in pc:
+        im_v = pc_dict[pc[line[start]]].copy()
+    
+    
+    for x in line[start+1::]:
         if(ord(x)==32):
             prev =   im_v.shape[1]
             space = np.zeros([im_v.shape[0],hw_dict[0].shape[1]],dtype=np.uint8)
             space.fill(255)
             im_v = cv2.hconcat([im_v,space])
-            for j in range(im_v.shape[0]):
-                im_v[j,prev-1] = 255
+            #for j in range(im_v.shape[0]):
+            #    im_v[j,prev-1] = 255
         elif(ord(x)>=97 and ord(x)<=122):  
             prev =   im_v.shape[1]
             im_v = cv2.hconcat([im_v,    cv2.resize(hw_dict[ord(x)-97], (hw_dict[ord(x)-97].shape[1], im_v.shape[0]))  ])
             
-            for j in range(im_v.shape[0]):
-                im_v[j,prev-1] = 255
+            #for j in range(im_v.shape[0]):
+            #    im_v[j,prev-1] = 255
+        elif( x in pc):
+            prev =   im_v.shape[1]
+            im_v = cv2.hconcat([im_v,    cv2.resize(pc_dict[pc[x]], (pc_dict[pc[x]].shape[1], im_v.shape[0]))  ])           
+            #for j in range(im_v.shape[0]):
+            #    im_v[j,prev-1] = 255           
     ans.append(im_v)
     if(im_v.shape[1]>max_len):
         max_len = im_v.shape[1]
@@ -62,5 +98,3 @@ while True:
     if cv2.waitKey(100) == 27:
         break
 cv2.destroyAllWindows()
-
-
